6bd7b906ec023a0489a92097303a8a710634ef7d
duong.duc <duc.dd108@gmail.com>
2024-11-19 16:22:38 +0700

    Add env firebase config

--- src/services/firebaseService.js
+++ src/services/firebaseService.js
@@ -1,8 +1,23 @@
-//const firebaseAdmin = require('firebase-admin');
 import firebaseAdmin from 'firebase-admin'
 
 //var serviceAccount = require("./public/js/firebase/config.json");
-import serviceAccount from "../../config/firebase-config.json" assert {type: 'json'}
+//import serviceAccount from "../../config/firebase-config.json" assert {type: 'json'}
+import dotenv from 'dotenv';
+dotenv.config();
+
+const serviceAccount = {
+    "type": "service_account",
+    "project_id": process.env.FIREBASE_PROJECT_ID,
+    "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+    "private_key": process.env.FIREBASE_PRIVATE_KEY,
+    "client_email": "firebase-adminsdk-cifj2@crawl-reddit.iam.gserviceaccount.com",
+    "client_id": process.env.FIREBASE_CLIENT_ID,
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL,
+    "universe_domain": "googleapis.com"
+  };
 firebaseAdmin.initializeApp({
     credential: firebaseAdmin.credential.cert(serviceAccount),
     databaseURL: "https://crawl-reddit.firebaseio.com"

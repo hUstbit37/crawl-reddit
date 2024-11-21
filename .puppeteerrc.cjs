7b35aac7971144aff4841b12f4501ae57e3984fd
duong.duc <duc.dd108@gmail.com>
2024-11-21 11:03:11 +0700

    Fix error Could not find Chrome v5

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -1,15 +1,17 @@
-import { join } from 'path';
+// import { join } from 'path';
 
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+// import { fileURLToPath } from 'url';
+// import { dirname } from 'path';
 
-// Lấy đường dẫn tuyệt đối đến thư mục chứa file hiện tại
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
+const { join } = require('path');
 /**
  * @type {import("puppeteer").Configuration}
  */
+console.log(1, __dirname);
+
 module.exports = {
   // Changes the cache location for Puppeteer.
   //cacheDirectory: join(__dirname, "node_modules", ".puppeteer_cache"),

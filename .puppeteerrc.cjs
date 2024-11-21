2dd0e0052c76f38a0326f332c4913fccb885cca3
duong.duc <duc.dd108@gmail.com>
2024-11-21 09:04:32 +0700

    Fix error Could not find Chrome v4

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -1,9 +1,17 @@
-const {join} = require('path');
+import { join } from 'path';
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+// Lấy đường dẫn tuyệt đối đến thư mục chứa file hiện tại
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 /**
  * @type {import("puppeteer").Configuration}
  */
 module.exports = {
   // Changes the cache location for Puppeteer.
-  cacheDirectory: join(__dirname, "node_modules", ".puppeteer_cache"),
+  //cacheDirectory: join(__dirname, "node_modules", ".puppeteer_cache"),
+  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
 };
